bc95fc5d5645be4f128c675b7c08d2e78318c7aa
Thiago Peluque <thiago.peluque@gmail.com>
2025-11-16 22:01:46 -0300

    Adição do username no Header

--- resources/views/topbar.blade.php
+++ resources/views/topbar.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="col">
         <div class="d-flex justify-content-end align-items-center">
-            <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>[username]</span>
+            <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>{{ session('user.username') }}</span>
             <a href="{{ route('logout') }}" class="btn btn-outline-secondary px-3">
                 Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
             </a>
